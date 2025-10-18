f8bd4060d5b61c90b0a349f2edb61d697918fb8b
infacundo2 <bamperri2000gmail.com>
2025-10-18 00:19:13 -0300

    Appsetings.josn correccion

--- obj/Debug/net8.0/AnimalCrossingTracker.AssemblyInfo.cs
+++ obj/Debug/net8.0/AnimalCrossingTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AnimalCrossingTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eeb1c651cb1050ef65c9d64c9f319399bc5e6e19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9357fbe6859b340a0b6a1496954b5afbaec76cf2")]
 [assembly: System.Reflection.AssemblyProductAttribute("AnimalCrossingTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AnimalCrossingTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
