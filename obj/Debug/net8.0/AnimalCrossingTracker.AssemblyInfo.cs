eeb1c651cb1050ef65c9d64c9f319399bc5e6e19
infacundo2 <bamperri2000gmail.com>
2025-10-18 00:08:10 -0300

    eliminacion

--- obj/Debug/net8.0/AnimalCrossingTracker.AssemblyInfo.cs
+++ obj/Debug/net8.0/AnimalCrossingTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AnimalCrossingTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+548b32c840060e6f657fe2a265056e6d4735c49d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f14c473d8c18abdc02bb4481e4c336d456790c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("AnimalCrossingTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AnimalCrossingTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
