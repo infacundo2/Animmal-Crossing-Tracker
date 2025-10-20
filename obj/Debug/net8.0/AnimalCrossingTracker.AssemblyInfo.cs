e324e4cc3a22f66fdecfeb4515ae36b57b8e1f96
infacundo2 <bamperri2000gmail.com>
2025-10-20 15:11:12 -0300

    Previsualizacion nav

--- obj/Debug/net8.0/AnimalCrossingTracker.AssemblyInfo.cs
+++ obj/Debug/net8.0/AnimalCrossingTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AnimalCrossingTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2906d811c6048ab5d9b1743f0a4c17908b281839")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12fe1c073803597f10653b82f9861fdada5a429d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AnimalCrossingTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AnimalCrossingTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
